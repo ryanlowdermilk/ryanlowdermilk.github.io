501726e1ca1e3c7df772e00466bed7b7828fbe2b
ryanlowdermilk <ryanlowdermilk@gmail.com>
2018-03-16 14:55:58 -0500

    added manifest.json support

--- service-worker.js
+++ service-worker.js
@@ -6,5 +6,5 @@
                 skipWaiting: true,
                 clientsClaim: true
             });
-            workboxSW.precache([{"url":"/404.html","revision":"66fe80026c96b7d7b1d19d98f64705ef"},{"url":"/about/index.html","revision":"ff6ff21c048950ffe48741f157563e7c"},{"url":"/index.html","revision":"a96e901f93e1704a6761969759592379"},{"url":"/jekyll/update/2018/03/16/welcome-to-jekyll.html","revision":"c24af9a273c7cb57d87e680d4fdd228a"},{"url":"/sw-register.js","revision":"9e0d49f2d503aa22f039c8f6fd1f1c2c"},{"url":"/assets/main.css","revision":"6c6b1de0fc20924a403482f02883b25b"},{"url":"/assets/minima-social-icons.svg","revision":"3a70b871c930a7ed8af27caa162af123"}]);
+            workboxSW.precache([{"url":"/404.html","revision":"dfe464624566edbb14b262628a13beff"},{"url":"/about/index.html","revision":"a473d0a6eec337667f429a9fdbcdf967"},{"url":"/index.html","revision":"c5fd46bea574e73410490356242ec959"},{"url":"/jekyll/update/2018/03/16/welcome-to-jekyll.html","revision":"726a97fd49192158a145e518d1a4e5a0"},{"url":"/sw-register.js","revision":"b6f02b616abfe53e7d1900eedb1fc0ed"},{"url":"/assets/main.css","revision":"6c6b1de0fc20924a403482f02883b25b"},{"url":"/assets/minima-social-icons.svg","revision":"3a70b871c930a7ed8af27caa162af123"}]);
             
